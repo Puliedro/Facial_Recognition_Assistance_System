8759cc2f6d25462a408de8883b905594f423f8b9
Karla <karladeav@gmail.com>
2024-02-11 14:40:34 -0600

    Students Data

---
+++ Populating students table.sql
@@ -0,0 +1,49 @@
+# Populating the students table 
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Hageo Juda', 'Balam', 'Mendez', '2109012@upy.edu.mx', '2109012', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Miguel Angel', 'Bastarrachea', 'Carballo', '2009009@upy.edu.mx', '2009009', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Angel Adrian', 'Campos', 'Borges', '2109021@upy.edu.mx', '2109021', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Samantha', 'Castro', 'Echeverria', '2109028@upy.edu.mx', '2109028', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Karla', 'Delgado', 'Avendaño', '2109050@upy.edu.mx', '2109050', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Julio Ernesto', 'Dzul', 'Dzib', '2009048@upy.edu.mx', '2009048', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Carlo Alejandro', 'Ek', 'Palomo', '2109058@upy.edu.mx', '2109058', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Juan Antonio', 'Fernandez', 'Cruz', '2109061@upy.edu.mx', '2109061', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Hector Emiliano', 'Gonzalez', 'Martinez', '2109077@upy.edu.mx', '2109077', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Luis David', 'Martínez', 'Gutiérrez', '2109099@upy.edu.mx', '2109099', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Sonia Estefania', 'Mendia', 'Martinez', '2109104@upy.edu.mx', '2109104', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Juliana Alejandra', 'Ramayo', 'Cardoso', '2109128@upy.edu.mx', '2109128', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Angel David', 'Sansores', 'Cruz', '2109139@upy.edu.mx', '2109139', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('José Manuel', 'Solano', 'Ochoa', '2009127@upy.edu.mx', '2009127', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Yahir Benjamin', 'Sulu', 'Chi', '2109145@upy.edu.mx', '2109145', '4°', 'A', 'Ingeniería de Datos');
+
+INSERT INTO students (firstname, surname_1, surname_2, email, schoolID, school_grade, school_group, degree)
+VALUES ('Priscila Guadalupe', 'Tzuc', 'Alonso', '2109148@upy.edu.mx', '2109148', '4°', 'A', 'Ingeniería de Datos');
